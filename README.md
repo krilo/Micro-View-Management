31f1ec4356c756f586814d8faaddc2639e168e0f
kristofer <kristofer@kristofers-MacBook-Pro.local>
2015-08-16 11:08:02 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # Micro-View-Management
 A Simple ViewManager for initing, opening and closing views.
+
+## Example Usage
+```javascript
+
+    var viewManager = new ViewManager();
+
+
+    function View(data) {
+      this.data = data;
+    };
+
+    View.prototype = {
+
+      init: function( data, done ) {
+        this.el = document.createElement('div')
+        this.el.classList.add('section')
+        this.el.innerHTML = "<p>View: "+this.data.title+"</p>"
+        document.body.appendChild(this.el)
+        done();
+      },
+
+      animateIn: function( data, done ) {
+        TweenMax.to(
+          this.el,
+          0.4,
+          {
+            opacity: 1,
+            onComplete: done
+          }
+        )
+      },
+
+      animateOut: function( data, done ) {
+        TweenMax.to(
+          this.el,
+          0.4,
+          {
+            opacity: 0,
+            onComplete: done
+          }
+        )
+      },
+
+      destroy: function( data, done ) {
+        this.el.parentNode.removeChild(this.el)
+        done();
+      }
+    };
+
+    var view1 = new View({title: "Home"});
+    var view2 = new View({title: "About"});
+```
---
+++ index.js
@@ -0,0 +1,112 @@
+class ViewManager {
+
+  constructor() {
+    this.currentView = null;
+    this.newView = null;
+  }
+
+  /*
+   *  Show View
+   */
+
+  show(view, onComplete, data = {}) {
+
+    this.data = data;
+
+    if( view != this.newView && view != this.currentView ) {
+
+      /*
+       * Destroy old view if needed
+       */
+
+      if( this.newView && this.newView.destroy ){
+        this.newView.destroy(this.data, function() { })
+      }
+
+      this.newView = view;
+      view.init( this.data, this.swap.bind( this, this.newView, onComplete ) );
+    }
+  }
+
+  /*
+   * Swap content
+   */
+
+  swap(newView, onComplete) {
+    if( newView == this.newView ) {
+      let oldView = this.currentView;
+      let onOldOut;
+
+      /**
+       * On new View in
+       */
+      let onNewIn = function() {
+        if( onComplete ) {
+          onComplete( newView, oldView );
+        }
+      };
+
+      /**
+       *  Bring in new View
+       */
+      let bringInNewView = function() {
+        this.currentView = newView;
+        this.newView = null;
+
+        if( newView.animateIn ) {
+          newView.animateIn( this.data, onNewIn );
+        } else {
+          onNewIn();
+        }
+      }.bind( this );
+
+      /**
+       * Bring out old View
+       */
+      let takeOutOldView = function() {
+
+        // if there's an animateOut function execute it on oldContent
+        if( oldView.animateOut ) {
+          oldView.animateOut( this.data, onOldOut );
+        } else {
+          onOldOut();
+        }
+      }.bind( this );
+
+      /**
+       * Destroy old View
+       */
+      let destroyOldView = function() {
+        if( oldView.destroy ) {
+          oldView.destroy( this.data, function() { } );
+        }
+      }.bind( this );
+
+      /**
+       * If View already visible
+       */
+      if( this.currentView ) {
+
+        onOldOut = function() {
+          destroyOldView();
+          bringInNewView();
+        }.bind(this);
+
+        takeOutOldView();
+
+      /**
+       * Else just show new content
+       */
+      } else {
+        bringInNewView();
+      }
+    }
+  }
+
+}
+
+if (typeof module != "undefined" && module.exports) {
+  module.exports = function instance(settings){
+    return new ViewManager(settings)
+  };
+}
